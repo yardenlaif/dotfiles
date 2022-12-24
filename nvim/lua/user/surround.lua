6798521ef0b46808b057d95faa07ceac6850513e
Yarden Laifenfeld <ylaifenfeld@gmail.com>
2022-12-24 15:33:29 +0200

    Don't use < for surround

--- nvim/lua/user/surround.lua
+++ nvim/lua/user/surround.lua
@@ -24,26 +24,43 @@ function Surround_with(start_surround, end_surround)
 end
 
 function Surround_with_input()
-	local start_input = vim.fn.input("Surround with> ", "")
-	local end_input = start_input
-	if string.find(end_input, "<") == 1 then
-		end_input = "</" .. string.sub(end_input, 2)
-	end
-
-	Surround_with(start_input, end_input)
-end
-
-function Increase_indentation()
 	local vstart = vim.fn.getpos("'<")
 	local vend = vim.fn.getpos("'>")
+	local start_column = vstart[3] - 1
 	local start_line = vstart[2]
+	local end_column = vend[3]
 	local end_line = vend[2]
-	local line = start_line
-	while line ~= end_line do
-		vim.api.nvim_win_set_cursor(0, { line, 0 })
-		vim.api.nvim_put({ "    " }, "c", true, true)
-		line = line + 1
+
+	-- Skip whitespaces at the start of lines
+	if start_column == 0 then
+		vim.api.nvim_win_set_cursor(0, { start_line, start_column })
+		local char = vim.api.nvim_get_current_line():sub(start_column + 1, start_column + 1)
+		while char == " " or char == "\t" do
+			start_column = start_column + 1
+			char = vim.api.nvim_get_current_line():sub(start_column + 1, start_column + 1)
+		end
 	end
+
+	local start_input = vim.fn.input("Surround with> ", "")
+	local end_input = start_input
+	if string.find(end_input, "<") == 1 and string.find(end_input, ">") == string.len(end_input) then
+		end_input = "</" .. string.sub(end_input, 2)
+	end
+	if string.len(end_input) == 1 then
+		if end_input == "<" then
+			end_input = ">"
+		end
+		if end_input == "(" then
+			end_input = ")"
+		end
+		if end_input == "{" then
+			end_input = "}"
+		end
+		if end_input == "[" then
+			end_input = "]"
+		end
+	end
+	Surround_with(start_input, end_input)
 end
 
 vim.keymap.set("v", "(", ":lua Surround_with('(', ')')<CR>", opts)
@@ -54,7 +71,6 @@ vim.keymap.set("x", '"', ":lua Surround_with('\"', '\"')<CR>", opts)
 vim.keymap.set("x", "'", ":lua Surround_with(''', ''')<CR>", opts)
 vim.keymap.set("x", "{", ":lua Surround_with('{', '}')<CR>", opts)
 vim.keymap.set("x", "}", ":lua Surround_with('{ ', ' }')<CR>", opts)
-vim.keymap.set("x", "<", ":lua Surround_with('<', '>')<CR>", opts)
-vim.keymap.set("x", ">", ":lua Surround_with('< ', ' >')<CR>", opts)
+-- vim.keymap.set('x', '<', ":lua Surround_with('<', '>')<CR>", opts)
+-- vim.keymap.set('x', '>', ":lua Surround_with('< ', ' >')<CR>", opts)
 vim.keymap.set("x", "S", ":lua Surround_with_input()<CR>", opts)
-vim.keymap.set("x", "\t", ":lua Increase_indentation()<CR>", opts)
