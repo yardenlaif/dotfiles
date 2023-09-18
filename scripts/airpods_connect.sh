0f619715725c92084a7ae53bfc0d4274755c0c71
Yarden Laifenfeld <ylaifenfeld@gmail.com>
2023-09-18 18:28:24 +0300

    Add airpords connect script

--- scripts/airpods_connect.sh
+++ scripts/airpods_connect.sh
@@ -1,1 +1,6 @@
-/home/yarden/scripts/airpods_connect.sh
+#!/bin/bash
+
+
+airpods=$(bluetoothctl devices 2>&1 | grep "Yarden’s AirPods Max" | cut -d" " -f2)
+command=$(bluetoothctl info $airpods | grep Connected | grep -q no && echo "connect" || echo "disconnect")
+bluetoothctl $command $airpods
