bd3833c283ffc29855315687586e056c3c4dc56b
Yarden Laifenfeld <ylaifenfeld@gmail.com>
2024-11-05 14:03:04 -0500

    Remove unused scripts

--- scripts/get_ip.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-ifconfig en0 | grep "inet " | grep -v 127.0.0.1 | cut -d\  -f2
--- scripts/globalprotect_toggle.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/zsh
-
-gpbin=$(which globalprotect)
-echo $gpbin
-sh -c "`which globalprotect` show --status"
-# command=$(sh globalprotect show --status | grep -q Connected && echo "disconnect" || echo "connect")
-# echo "$command"
-# globalprotect "$command"
